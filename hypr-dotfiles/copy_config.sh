c48fa45db847fd0cd89df2bc859f90913bee2e6e
obesnard001 <owen.besnard-oravec@etud.univ-pau.fr>
2024-10-28 00:31:44 +0100

    Added Sway dotfiles

---
+++ hypr-dotfiles/copy_config.sh
@@ -0,0 +1,1 @@
+rsync -av --exclude='./apps.txt' --exclude='./install_apps.txt' --exclude='./other.txt' --exclude='./copy_config.sh' ./* ~/.config/
---
+++ sway-dotfiles/copy-config.sh
@@ -0,0 +1,1 @@
+rsync -av --exclude='./requirements.txt' --exclude='./install_apps.txt' --exclude='./other.txt' --exclude='./copy-config.sh' ./* ~/.config/
